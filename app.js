49e4f2da25825e95f5e771e808ee7d4013b9810d
Clarence Castillo <clarencecastillo@outlook.com>
2020-05-19 16:35:45 +0800

    Mount config middlewares in app

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 var express = require('express');
 var logger = require('morgan');
+var bodyParser = require('body-parser');
+var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
+app.use(cors());
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // mount routers
 app.use('/', indexRouter);
